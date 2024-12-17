da0ca6dfa74c42987582d85bd652c683bd14ab9b
Ciaran Turner <76225695+ImmortalPhoenix23@users.noreply.github.com>
2024-12-16 22:24:16 -0800

    started minesweeper framework

--- main.cpp
+++ main.cpp
@@ -2,8 +2,31 @@
 #include <string>
 using namespace std;
 
+class display{
+    private:
+        int rows;
+        int cols;
+    public:
+        display();
+        int get_rows();
+        int get_cols();
+};
+
 int main(){
-    cout << "Hello world" << endl;
-    cout << "more";
-    cout << "hello hello" << endl;
+    display* displayOne = new display();
+
+}
+
+int display::get_rows()
+{
+}
+
+int display::get_cols()
+{
+}
+
+display::display()
+{
+    rows = 0;
+    cols = 0;
 }
